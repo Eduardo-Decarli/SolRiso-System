93103163599bf9d1757754cfe268b7aadedc0469
Eduardo-Decarli <decarliedu@gmail.com>
2025-03-02 16:02:30 -0300

    feat: controllers for reservations

---
+++ src/main/java/com/decarli/solriso_system/model/controller/ReservationController.java
@@ -0,0 +1,44 @@
+package com.decarli.solriso_system.model.controller;
+
+import com.decarli.solriso_system.model.entities.Reservation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/reservation")
+public class ReservationController {
+
+    @PostMapping
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getReservationsByToday() {
+        return null;
+    }
+
+    @GetMapping("/byRoom")
+    public ResponseEntity<List<Reservation>> getReservationByRoom(@PathVariable int roomNumber) {
+        return null;
+    }
+
+    @GetMapping("/byResponsableName")
+    public ResponseEntity<List<Reservation>> getReservationByResponsableName(@PathVariable String responsableName) {
+        return null;
+    }
+
+    @PutMapping
+    public ResponseEntity<Reservation> updateReservation(@RequestBody Reservation reservation) {
+        return null;
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Reservation> deleteReservation(@PathVariable UUID id) {
+        return null;
+    }
+
+}
--- src/main/java/com/decarli/solriso_system/model/entities/Reservation.java
+++ src/main/java/com/decarli/solriso_system/model/entities/Reservation.java
@@ -4,10 +4,12 @@ import com.decarli.solriso_system.model.enums.TypeReservation;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 public class Reservation {
 
+    private UUID id;
     private int roomNumber;
     private int quantGuests;
     private TypeReservation typeReservation;
