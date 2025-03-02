65ee42f27c4b01a80fb04f1f6d5ac514cf0528ac
Eduardo-Decarli <decarliedu@gmail.com>
2025-03-02 15:21:12 -0300

    feat: import the project

---
+++ src/main/java/com/decarli/solriso_system/SolrisoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.decarli.solriso_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrisoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrisoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decarli/solriso_system/SolrisoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decarli.solriso_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrisoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
