644705efbaf73eef7e510abefe1dc047cf169e0b
Eduardo-Decarli <decarliedu@gmail.com>
2025-03-13 15:54:28 -0300

    feat: regex for password

--- src/main/java/com/decarli/solriso_system/model/dto/admin/AdminCreateDto.java
+++ src/main/java/com/decarli/solriso_system/model/dto/admin/AdminCreateDto.java
@@ -1,6 +1,7 @@
 package com.decarli.solriso_system.model.dto.admin;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -11,6 +12,7 @@ public class AdminCreateDto {
     @NotNull(message = "Email can't be null")
     private String email;
     @NotNull(message = "Password can't be null")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "The password has be eight characters, a uppercase, a lowercase, a number and a special character")
     private String password;
     @NotNull(message = "Role can't be null")
     private String role;
