3a05735006af0a3f242d34e5148b14568f004d8f
Eduardo-Decarli <decarliedu@gmail.com>
2025-06-13 19:34:52 -0300

    feat: Create initials config about SLF4J

---
+++ Backend/src/main/java/com/decarli/solriso_system/utils/LoggerUtil.java
@@ -0,0 +1,10 @@
+package com.decarli.solriso_system.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+    public static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+
+
+}
