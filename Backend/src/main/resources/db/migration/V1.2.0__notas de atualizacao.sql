18eae2b72c7f454fafda0a3603c0a6d0d95df3be
Eduardo-Decarli <decarliedu@gmail.com>
2026-01-07 20:13:13 -0300

    refactor: implements a better organization in the flyway

--- Backend/src/main/resources/db/migration/V1.2.0__notas de atualizacao.sql
+++ Backend/src/main/resources/db/migration/V1.2.0__notas de atualizacao.sql
@@ -1,11 +1,11 @@
-INSERT INTO guesthouse.HISTORIC (VERSION, DESCRIPTION)
+INSERT INTO HISTORIC (VERSION, DESCRIPTION)
     VALUES ('1.0.0', 'Criação e estruturação do banco de dados');
 
-INSERT INTO guesthouse.HISTORIC (VERSION, DESCRIPTION)
+INSERT INTO HISTORIC (VERSION, DESCRIPTION)
 VALUES ('1.1.0', 'Criação de roles de usuários do sistema, dividindo em 3 grupos diferentes');
 
-INSERT INTO guesthouse.HISTORIC (VERSION, DESCRIPTION)
+INSERT INTO HISTORIC (VERSION, DESCRIPTION)
 VALUES ('1.2.0', 'Criação de notas de atualizações para o sistema e rota de acesso');
 
-ALTER TABLE guesthouse.HISTORIC
+ALTER TABLE HISTORIC
     ADD CONSTRAINT unique_version UNIQUE (VERSION);
--- Backend/src/main/resources/db/migration/V1.3.0__login de usuário.sql
+++ Backend/src/main/resources/db/migration/V1.3.0__login de usuário.sql
@@ -1,2 +1,2 @@
-INSERT INTO guesthouse.HISTORIC (VERSION, DESCRIPTION)
+INSERT INTO HISTORIC (VERSION, DESCRIPTION)
     VALUES ('1.3.0', 'Tela e Sistema de Login para usuários acessarem');
